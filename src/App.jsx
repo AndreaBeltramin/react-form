3d26334687c95cd3cc1fff16f4ae40d92aef15a1
AndreaBeltramin <andreabeltra99@gmail.com>
2024-12-03 14:39:34 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+	const [count, setCount] = useState(0);
+
+	return (
+		<>
+			<div className="container mt-5">
+				<h1>ciao</h1>
+			</div>
+		</>
+	);
+}
+
+export default App;
