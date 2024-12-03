817de5b671e506f9b408efd9ba475d0ea0855da3
AndreaBeltramin <andreabeltra99@gmail.com>
2024-12-03 16:58:11 +0100

    creazione form con aggiunta di titoli

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 import { useState } from "react";
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [list, setList] = useState([]);
+	const [addTitle, setAddTitle] = useState("");
 
+	const handleInputChange = (e) => {
+		setAddTitle(e.target.value);
+	};
+	const handleFormSubmit = (e) => {
+		e.preventDefault();
+		console.log("hai aggiunto " + addTitle);
+		const newList = [...list, addTitle];
+		setList(newList);
+	};
 	return (
 		<>
 			<div className="container mt-5">
-				<h1>ciao</h1>
+				<h1>Blog </h1>
+				<form onSubmit={handleFormSubmit}>
+					<input type="text" value={addTitle} onChange={handleInputChange} />
+					<button className="btn btn-primary ms-2">
+						Aggiungi titolo alla lista
+					</button>
+				</form>
+				<ul>
+					{list.map((el, index) => (
+						<li key={index}>{el}</li>
+					))}
+				</ul>
 			</div>
 		</>
 	);
